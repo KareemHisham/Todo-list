7eee711249f6e24cd60ea84754027dada567810d
KareemHisham <kareem.hisham.ahmed@gmail.com>
2019-10-10 14:26:55 +0200

    Updates

---
+++ precache-manifest.bb202314dced62146c104a8ff1609885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b5d09d8f73ca3684d8fa86ca68a353",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "15ef0f2e8426562aeb3e",
+    "url": "/Todo-list/static/css/main.98e279b5.chunk.css"
+  },
+  {
+    "revision": "a76cd2631ae6944d5939",
+    "url": "/Todo-list/static/js/2.fe490230.chunk.js"
+  },
+  {
+    "revision": "15ef0f2e8426562aeb3e",
+    "url": "/Todo-list/static/js/main.3a2d4dc0.chunk.js"
+  },
+  {
+    "revision": "c1efcbd3d7faf29383ba",
+    "url": "/Todo-list/static/js/runtime-main.ff2b1d89.js"
+  }
+]);
